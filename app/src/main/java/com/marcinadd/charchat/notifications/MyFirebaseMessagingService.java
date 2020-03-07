17911a7c47ea2dfae984a610bbb53c87852c49ae
marcinadd <marcinadd@gmail.com>
2020-03-07 16:47:59 +0100

    Implement notification grouping

--- app/src/main/java/com/marcinadd/charchat/notifications/MyFirebaseMessagingService.java
+++ app/src/main/java/com/marcinadd/charchat/notifications/MyFirebaseMessagingService.java
@@ -2,7 +2,10 @@ package com.marcinadd.charchat.notifications;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Build;
+import android.os.SystemClock;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
@@ -10,11 +13,12 @@ import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.marcinadd.charchat.NavigationDrawerActivity;
 import com.marcinadd.charchat.R;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private final static String CHANNEL_ID = "charchat_chat_messages";
-
+    private final static String GROUP_KEY_CHAT = "com.marcinadd.charchat.CHAT";
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
@@ -23,23 +27,28 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     public void createNotificationFromRemoteMessage(RemoteMessage remoteMessage) {
+        Intent intent = new Intent(this, NavigationDrawerActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_menu_send)
                 .setContentTitle(remoteMessage.getNotification().getTitle())
                 .setContentText(remoteMessage.getNotification().getBody())
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setGroup(GROUP_KEY_CHAT);
 
         createNotificationChannel();
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
-        notificationManagerCompat.notify(563, builder.build());
+        notificationManagerCompat.notify((int) SystemClock.uptimeMillis() % 99999999, builder.build());
     }
 
     public void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = CHANNEL_ID;
             String description = "Chat messages";
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_ID, importance);
             channel.setDescription(description);
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
